af339a02eb25787fc241ad835d3eea36b9817245
fukushima_satsuki <fukushima.satsuki@kindai.ac.jp>
2022-02-11 16:24:43 +0900

    改良

--- main.js
+++ main.js
@@ -1,58 +1,35 @@
-const quizText = document.getElementById("quiz-text");
-const quizImage = document.getElementById("quiz-image");
-const choice1 = document.getElementById("choice-1");
-const choice2 = document.getElementById("choice-2");
-const choice3 = document.getElementById("choice-3");
-const feedback = document.getElementById("feedback");
-
-// クイズの内容
-const quiz = {
-  image: "./image/",
-  choices: [
-    {
-      text: "上海",
-      feedback: "残念！",
+Vue.createApp({
+  data() {
+    return {
+      feedback: "",
+      quiz: {
+        text: "私の昔住んでいたこの場所はどこでしょう❓",
+        image: "LINE_ALBUM_20160509 _1_211115_101.jpg",
+        logs: [
+          {
+            text: "蘇州",
+            feedback: "正解！東洋のベネチアと呼ばれている運河の街です",
+          },
+          {
+            text: "上海",
+            feedback: "残念！でも近いのでめっちゃ行きました",
+          },
+          {
+            text: "北京",
+            feedback: "残念！家族旅行で一度行ったことがあります",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    choiced(log) {
+      this.feedback = log.feedback;
     },
-    {
-      text: "北京",
-      feedback: "残念！",
+  },
+  computed: {
+    quizImagePath() {
+      return "./image/" + this.quiz.image;
     },
-    {
-      text: "蘇州",
-      feedback: "正解！",
-    },
-  ],
-};
-
-// quiz を画面に表示する関数
-const reloadQuiz = function () {
-  // 画像を表示
-  quizImage.src = "./image/" + quiz.image;
-
-  // 選択肢（ボタン）の中身を表示
-  choice1.textContent = quiz.choices[0].text;
-  choice2.textContent = quiz.choices[1].text;
-  choice3.textContent = quiz.choices[2].text;
-};
-
-// choiceNumber番目の選択肢を選択
-const choose = function (choiceNumber) {
-  // フィードバックを表示
-  feedback.textContent = quiz.choices[choiceNumber].feedback;
-};
-
-choice1.onclick = function () {
-  // 0 番目の選択肢を選択
-  choose(0);
-};
-choice2.onclick = function () {
-  // 1 番目の選択肢を選択
-  choose(1);
-};
-choice3.onclick = function () {
-  // 2 番目の選択肢を選択
-  choose(2);
-};
-
-// reloadQuiz関数 を実行して、クイズを画面に表示する
-reloadQuiz();
+  },
+}).mount("#omake");
